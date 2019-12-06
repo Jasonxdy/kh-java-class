e357bb2404e8e4d1009733b7b2d4a095fcc46dd3
jdy <dicobomb@naver.com>
2019-12-06 17:48:37 +0900

    19/12/06

---
+++ 3_JDBC/myTestEBook/BUY.sql
@@ -0,0 +1,17 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 금요일-12월-06-2019   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table BUY
+--------------------------------------------------------
+
+  CREATE TABLE "MEMBER"."BUY" 
+   (	"MEMBER_ID" VARCHAR2(30 BYTE), 
+	"BOOK_NO" NUMBER
+   ) SEGMENT CREATION IMMEDIATE 
+  PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1 BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+REM INSERTING into MEMBER.BUY
+SET DEFINE OFF;
