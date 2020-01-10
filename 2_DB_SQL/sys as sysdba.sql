242c4f5f5655c1d53065e5515b25ae305897d13d
Jasonxdy <57127841+Jasonxdy@users.noreply.github.com>
2020-01-10 17:48:08 +0900

    20/01/10

--- 2_DB_SQL/sys as sysdba.sql
+++ 2_DB_SQL/sys as sysdba.sql
@@ -3,3 +3,7 @@ select username from dba_users;
 CREATE USER kh IDENTIFIED BY kh; /*kh라는 계정을 kh라는 비밀번호로 생성*/
 GRANT RESOURCE, CONNECT TO kh; /*resource(객체)에 대한 권한(role)을 부여*/
 /*CONNECT : 권한들을 말아서 모아놓음*/
+
+
+CREATE USER server IDENTIFIED BY server;
+GRANT CONNECT, RESOURCE TO server;
