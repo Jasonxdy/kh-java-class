f68b3a5d1df113c634ebe07e07e3d9c16405e46b
Jasonxdy <57127841+Jasonxdy@users.noreply.github.com>
2020-03-29 14:49:52 +0900

    commit

---
+++ 1_java/TestProject/Test/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("|\\_/|\r\n" + 
+				"|q p|   /}\r\n" + 
+				"( 0 )\"\"\"\\\r\n" + 
+				"|\"^\"`    |\r\n" + 
+				"||_/=\\\\__|");
+	}
+}
